a96a96b24248b7b491015db726fdcedcdde7a3d7
uries242 <teykoa@gmail.com>
2023-11-18 16:52:35 -0500

    Initial Commit

---
+++ ApplePie/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by Teyko on 11/18/23.
+//
+
+import Foundation
+
+struct Game {
+    var word : String
+    var incorrectMovesRemaining : Int
+    
+    
+}
--- ApplePie/ViewController.swift
+++ ApplePie/ViewController.swift
@@ -1,19 +1,59 @@
-//
-//  ViewController.swift
-//  ApplePie
-//
-//  Created by Teyko on 11/14/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var score: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
+    
+    func newRound(){
+        
+    }
+    
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
+    var listOfWords = ["buccaneers", "swift", "glorius", "incandescent", "bug", "program" ]
+    
+   let incorrectMovesAllowed = 7
+   var totalWins = 0
+    var totalLosses = 0
+    
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
+// end page 
 
